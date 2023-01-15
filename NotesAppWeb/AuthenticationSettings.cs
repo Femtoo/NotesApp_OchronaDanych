ab7c9f4ba2c4b36313665ef9964628f57b9c1ee7
Konrad Trusiewicz <konrad1.tr@gmail.com>
2023-01-15 18:32:05 +0100

    ssl bugfixing

--- NotesAppWeb/AuthenticationSettings.cs
+++ NotesAppWeb/AuthenticationSettings.cs
@@ -3,8 +3,8 @@
     public static class AuthenticationSettings
     {
         public static string JwtKey { get; set; } = "sauidhufihaiuhfuidshfuihdfuisdhiugdsuighsuidhfuisdhuisdhufigsdhuighsduihgui";
-        public static string Issuer { get; set; } = "https://localhost:7236;http://localhost:5074";
-        public static string Audience { get; set; } = "https://localhost:7236;http://localhost:5074";
+        public static string Issuer { get; set; } = "https://localhost/";
+        public static string Audience { get; set; } = "https://localhost/";
         public static double JwtExpiresDays { get; set; } = 1;
     }
 }
